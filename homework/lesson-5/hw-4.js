adca23e0ed36457f48daa0a867f2ef8f2bd9f8f1
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2021-08-09 00:45:39 +0300

    Update hw-4.js

--- homework/lesson-5/hw-4.js
+++ homework/lesson-5/hw-4.js
@@ -10,8 +10,14 @@ isEmpty(obj);
 
 
 //Task-2
+var x = prompt('Введите число которое хотите возвести в степень'),
+	n = prompt('Введите степень');
+while(isNaN(n) || n < 0 || (n % parseInt(n) !==0)) {
+	alert('Степень возведения должна быть натуральным числом');
+	n = prompt('Введите степень');
+}
 function pow(x, n) {
-	if(isNaN(x) || x === 0 || isNaN(n) || n < 0) {
+	if(isNaN(x) || x === 0) {
 		return false;
 	}
 	if(n === 1) {
@@ -20,7 +26,8 @@ function pow(x, n) {
 		return x * pow(x, --n);
 	}
 }
-console.log(pow(2, 3));
+console.log(pow(x, n));
+
 
 
 //Task-3
@@ -43,7 +50,10 @@ sumTo(3);
  а потом возвращает значение из каждой рекурсии.
  Т. е. как я понял происходит накидывание функций для выполнения в корзину,
  как только дошло до того, что функция выполнилась(что то вернула) начинаем вытаскивать из
- корзины функции и с каждой получать значение*/
+ корзины функции и с каждой получать значение
+
+ sumTo(100000) не посчитает, так как в браузере стоит ограничение на <= 10000 вложенных рекурсий
+ */
 function sumTo(n) {
 	if(+n && n > 0) {
 		return (n === 1) ? n : n + sumTo(--n);
@@ -66,17 +76,21 @@ sumTo(3);
 //Task-4
 function treeSum(arr, sum) {
 	for(var key in arr) {
-		if(typeof(arr[key] !== null && arr[key]) === 'object' && arr[key].length) {
-			sum += treeSum(arr[key], 0);
-		}
-		if(typeof arr[key] !== 'number') continue;
-		else{
+		if( typeof(arr[key]) === 'number' && isFinite(arr[key]) ) {
 			sum += arr[key];
+		}
+		if( arr[key] !== null && typeof(arr[key]) === 'object' && arr[key].length ) {
+			sum += treeSum(arr[key], 0);
 		}
 	}
 	return sum;
 }
-console.log(treeSum([5, 7, [4, null, undefined,'dsfasdf', '', '423423sdfdf', [2], 8, [1, null, undefined,'dsfasdf', '', '423423sdfdf', 3], 2], [9, []], 1, 8, null, {}, {name : 'Igor'}, undefined,'dsfasdf', '', '423423sdfdf'], 0));
+console.log(treeSum([
+	5, 7, 'ffdf', '', 0, null, undefined, true, false, {}, {name: 'ddd'}, NaN, Infinity,
+	[4, [2], 8, [1, 3], 2],
+	[9, []],
+	1, 8
+], 0));
 
 
 
