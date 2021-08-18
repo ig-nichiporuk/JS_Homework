52a6eb50066046fd5f2da9496d7cc0b9de3bd11f
IgorNichiporuk <igor.nichiporuk@autoset.by>
2021-08-18 19:33:34 +0300

    fix-1 hw-6

--- homework/lesson-9/hw-6/main.js
+++ homework/lesson-9/hw-6/main.js
@@ -1,30 +1,31 @@
 function Animals(name) {
 	var DAILY_FOOD_MIN = 50,
 		DAILY_FOOD_MAX = 500,
-		foodAmount = 20;
+		foodAmount = 50;
 	this.name = name;
 
 	function formatFoodAmount() {
 		return foodAmount + ' гр.';
 	}
-	this.dailyNorm = function (foodAmount) {
+	this.dailyNorm = function (amount) {
 		if (!arguments.length) {
 			return formatFoodAmount();
 		}
-		if (!(typeof foodAmount === 'number') || !isFinite(foodAmount)) {
+		if (!(typeof amount === 'number') || !isFinite(amount)) {
 			throw new Error('Вы сыпете что-то не понятное!');
 		}
-		if ((typeof foodAmount === 'number') && isFinite(foodAmount) && foodAmount < DAILY_FOOD_MIN) {
+		if ((typeof amount === 'number') && isFinite(amount) && amount < DAILY_FOOD_MIN) {
 			throw new Error('Вашего питомца вы точно не порадуете! Насыпте не меньше ' + DAILY_FOOD_MIN + ' гр.');
 		}
-		if ((typeof foodAmount === 'number') && isFinite(foodAmount) && foodAmount > DAILY_FOOD_MAX) {
+		if ((typeof amount === 'number') && isFinite(amount) && amount > DAILY_FOOD_MAX) {
 			throw new Error('Ваш питомец станет толстым! Не сыпте больше ' + DAILY_FOOD_MAX + ' гр.');
 		}
+		foodAmount = amount;
 	};
 
 	var self = this;
 	this.feed = function () {
-		console.log('Насыпаем в миску ' + self.dailyNorm(foodAmount) + ' гр. корма.');
+		console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
 	};
 }
 
@@ -45,5 +46,6 @@ function Cat(name) {
 }
 
 var cat = new Cat('Vasya');
+cat.dailyNorm(60);
 console.log(cat.name);
-console.log(cat.stroke().feed());
+console.log(cat.feed().stroke());
