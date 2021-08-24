a1226db106761076bd3a0d2a620703dc7bcf1978
igor.nichiporuk <ig.nichiporuk@gmail.com>
2021-08-24 22:24:37 +0300

    practice-12

---
+++ practice/lesson-12/main.js
@@ -0,0 +1,8 @@
+var p1 = document.createElement('p'),
+	p2 = document.createElement('p'),
+	parent = document.getElementById('id');
+parent.appendChild(p1);
+parent.appendChild(p2);
+p1.innerHTML = 'Links: <a href="#">Link1</a> <a href="#">Link2</a>';
+p2.innerHTML = 'Links: <a href="#">Link3</a> <a href="#">Link4</a>';
+
