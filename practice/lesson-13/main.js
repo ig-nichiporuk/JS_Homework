717bbfe536dcf3504db407e13e8b6c67b61ebecf
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2021-09-01 18:31:30 +0300

    fix practice-13

--- practice/lesson-13/main.js
+++ practice/lesson-13/main.js
@@ -7,4 +7,3 @@ p1.innerHTML = 'Links: <a href="#">Link1</a> <a href="#">Link2</a>';
 p2.innerHTML = 'Links: <a href="#">Link3</a> <a href="#">Link4</a>';
 parent.appendChild(p1);
 parent.appendChild(p2);
-
