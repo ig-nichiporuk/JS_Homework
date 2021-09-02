2e31b86e4e06fb87c887b744dd7254f16b8de387
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2021-09-02 13:51:21 +0300

    fix hw-10 - 1

--- homework/lesson-14/hw-10/main.js
+++ homework/lesson-14/hw-10/main.js
@@ -1,45 +1,34 @@
 var table = document.getElementById('table'),
 	button = table.getElementsByClassName('btn')[0],
 	rows = table.getElementsByTagName('tr');
-
 function addRow(){
 	var newRow = document.createElement('tr');
-
 	newRow.innerHTML = '<td></td><td></td><td></td>';
 	table.children[0].insertBefore(newRow, rows[0]);
 }
-
 function blurInput(input) {
-	input.addEventListener('blur', function (){
-		if(input.value) {
-			input.parentElement.dataset.status = 'edited';
+	input.addEventListener('keyup', function (e){
+		if(e.keyCode === 13) {
+			if(input.value) {
+				input.parentElement.dataset.status = 'edited';
+			}
+			else {
+				delete input.parentElement.dataset.status;
+			}
+			input.parentElement.innerHTML = input.value;
 		}
-		else {
-			delete input.parentElement.dataset.status;
-		}
-		input.parentElement.innerHTML = input.value;
 	}, false);
 }
-
-/*function enterInput(e) {
-	if(e.keyCode === 13) {
-		blurInput(input);
-	}
-}*/
-
 function addInput(e) {
 	var target = e.target,
 		newInput = document.createElement('input');
 	newInput.type = 'text';
-
 	if (target.tagName == 'TD' && !target.classList.contains('btn')) {
 		if(target.dataset.status) {
 			newInput.value = target.textContent;
 			target.textContent = '';
 			target.appendChild(newInput).focus();
 			var input = table.getElementsByTagName('input')[0];
-			input.addEventListener('blur', function (){
-			}, false);
 			blurInput(input);
 		}
 		else {
@@ -49,9 +38,27 @@ function addInput(e) {
 		}
 	}
 }
+function test(e) {
+	var target = e.target;
+	if (target.tagName !== 'input') {
+		var inputs = table.getElementsByTagName('input');
+		for (var i in inputs) {
+			inputs[i].onblur = function(e) {
+				if(e.target.value) {
+					e.target.parentElement.dataset.status = 'edited';
+				}
+				else {
+					delete e.target.parentElement.dataset.status;
+				}
+				e.target.parentElement.innerHTML = e.target.value;
+			};
+		}
+	}
+}
 
 button.addEventListener('click', addRow, false);
 table.addEventListener('click', addInput, false);
+document.addEventListener('click', test, false);
 
 
 
