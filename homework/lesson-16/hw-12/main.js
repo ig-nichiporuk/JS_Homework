6b32d84521494020103d62116487acad3641d8a9
IgorNichiporuk <igor.nichiporuk@autoset.by>
2021-09-28 16:38:58 +0300

    fix - 1 hw-12

--- homework/lesson-16/hw-12/main.js
+++ homework/lesson-16/hw-12/main.js
@@ -2,51 +2,54 @@ var users = document.getElementById('users'),
 	btn = document.getElementsByTagName('button')[0];
 
 function getUserData() {
-	try {
-		var usersInfo = JSON.parse(localStorage.getItem('users')).data,
-			usersTab = users.getElementsByClassName('usersTabsJS')[0],
-			usersTContent = users.getElementsByClassName('usersContentJS')[0];
+	var usersInfo = JSON.parse(localStorage.getItem('users')).data,
+		usersTabs = users.getElementsByClassName('usersTabsJS')[0],
+		usersContent = users.getElementsByClassName('usersContentJS')[0];
 
-		btn.disabled = true;
+	btn.disabled = true;
 
-		for (var i in usersInfo) {
-			usersTab.insertAdjacentHTML('beforeend',
-				'<span class="'+ (i == 0 ? 'active users_tab userTabJS' : 'users_tab userTabJS') +'" data-id='+ usersInfo[i].id +'>Пользователь' + ++i +'</span>')
-		}
-		usersTContent.insertAdjacentHTML('beforeend',
-			'<img src='+usersInfo[0].avatar+' alt="user-photo" class="users__photo userAvatarJS">' +
-			'<div class="users_info userInfoJS">' +
-			'<p class="name userNameJS">First Name: '+ usersInfo[0].first_name +'</p>' +
-			'<p class="suname userSunameJS">Last Name: '+ usersInfo[0].last_name +'</p>' +
+	for (var i in usersInfo) {
+		usersTabs.insertAdjacentHTML('beforeend',
+			'<span class="'+ (i == 0 ? 'active users__tab userTabJS' : 'users__tab userTabJS') +'" data-id='+ usersInfo[i].id +'>Пользователь ' + ++i +'</span>')
+	}
+	usersContent.insertAdjacentHTML('beforeend',
+		'<div class="users__content-wrap">' +
+				'<img src="'+usersInfo[0].avatar+'" alt="user-photo" class="users__photo userAvatarJS" width="130" height="130">' +
+				'<div class="users__info userInfoJS">' +
+					'<p class="name">First Name: <span class="userNameJS">'+ usersInfo[0].first_name +'</span></p>' +
+					'<p class="suname">Last Name: <span class="userSunameJS">'+ usersInfo[0].last_name +'</span></p>' +
+				'</div>' +
 			'</div>'
-		);
-		users.onclick = function (e) {
-			if (e.target.classList.contains('userTabJS') && !e.target.classList.contains('active')) {
-				var tabs = usersTab.getElementsByClassName('userTabJS');
-				for (var i = 0;  i < tabs.length; i++) {
-					tabs[i].classList.remove('active');
-				}
-				e.target.classList.add('active');
+	);
+	users.onclick = function (e) {
+		if (e.target.classList.contains('userTabJS') && !e.target.classList.contains('active')) {
+			var tabs = usersTabs.getElementsByClassName('userTabJS');
+			for (var i = 0;  i < tabs.length; i++) {
+				tabs[i].classList.remove('active');
 			}
-			for (var i in usersInfo) {
-				if(usersInfo[i].id == e.target.dataset.id) {
-					var userAvatar = users.getElementsByClassName('userAvatarJS')[0],
-						userName = users.getElementsByClassName('userNameJS')[0],
-						userSuname = users.getElementsByClassName('userSunameJS')[0];
-					userAvatar.src = usersInfo[i].avatar;
-					userName.textContent = 'First Name: '+ usersInfo[i].first_name;
-					userSuname.textContent = 'Last Name: '+ usersInfo[i].last_name;
-					break;
-				}
+			e.target.classList.add('active');
+		}
+		for (var j in usersInfo) {
+			if(usersInfo[j].id == e.target.dataset.id) {
+				var userAvatar = users.getElementsByClassName('userAvatarJS')[0],
+					userName = users.getElementsByClassName('userNameJS')[0],
+					userSuname = users.getElementsByClassName('userSunameJS')[0];
+				userAvatar.src = usersInfo[j].avatar;
+				userName.textContent = usersInfo[j].first_name;
+				userSuname.textContent = usersInfo[j].last_name;
+				break;
 			}
 		}
-		throw { name:'MyError', message:'что-то пошло не так!' };
-	}
-	catch(err) {
-		console.log(err.name);
-		console.log(err.message );
 	}
 
+}
+function errorMessageStatus(xhr) {
+	btn.remove();
+	users.insertAdjacentHTML('beforeend','<div class="error">' + '' +
+		'<p>Извините, что-то пошло&nbsp;не&nbsp;так</p>' +
+		'<span>'+ xhr.status +'</span>' +
+		'</div>'
+	);
 }
 
 btn.onclick = function () {
@@ -63,12 +66,16 @@ btn.onclick = function () {
 		xhr.onload = function () {
 			var statusType = +String(this.status)[0];
 
-			if (statusType == 2) {
+			if (statusType === 2) {
 				localStorage.setItem('users', this.response);
 				getUserData();
 			} else {
-				console.error(this.status);
+				errorMessageStatus(xhr);
 			}
+		}
+
+		xhr.onerror = function () {
+			errorMessageStatus(xhr);
 		}
 	}
 
