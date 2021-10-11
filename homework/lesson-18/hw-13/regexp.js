eb2b00e001f9dfb938fb70bcdf11256e5c405df7
igor.nichiporuk <ig.nichiporuk@gmail.com>
2021-10-11 21:55:19 +0300

    fix hw-13

--- homework/lesson-18/hw-13/regexp.js
+++ homework/lesson-18/hw-13/regexp.js
@@ -1,18 +1,12 @@
 //Task-1
-// var regexp = /^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@(?=[a-z\d])(?=[\.\-]*?[a-z\d])[a-z\d\.\-]{2,20}(?<![\.\-]).com$/gim;  //поиск в строке
-// var regexp = /[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@(?=[a-z\d])(?=[\.\-]*?[a-z\d])[a-z\d\.\-]{2,20}(?<![\.\-]).com/gim;       //поиск совпадения
+// var regexp = /^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@(?=[a-z\d])(?=[\.\-]*?[a-z\d])[a-z\d\.\-]{2,20}(?<![\.\-]).com$/gim;
+var regexp = /^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@[a-z\d]{1,10}[\.\-]{1}[a-z\d]{1,10}.com$/gim;
+console.log(regexp.test('igor_nichiporuk@12345678qw.123456789.com'));
 
-
-var regexp = /^@(?=[a-z\d])[\.]?(?=[a-z\d])[a-z\d\.\-]{2,20}$/gim;
-console.log(regexp.test('@12345678901234567.9'));
-// console.log(str.match(regexp));
-
-
-/*
 
 //Task-2
 function isPhone(phone){
-	var regexp = /^(\+?375\-?|8\-?0)(2[5|9]|33|44|17)-?[1-9]{3}-?\d{2}-?\d{2}$/;
+	var regexp = /^(\+?375\-?|8\-?0)(2[5|9]|33|44|17)-?(?!0)[0-9]{3}(-?\d{2}){2}$/;
 	console.log(regexp.test(phone));
 }
 isPhone('8-044-444-44-44');
@@ -54,11 +48,3 @@ function countVowelLetters(text) {
 }
 countVowelLetters('Шла Саша по шоссе И сосала сУшку'); // 12
 // countVowelLetters('Шл Сш п шсс ссл сшк');
-*/
-
-
-
-
-
-
-
