b46d8df12fe3423aaeeb4a9d5d65920102e7ed77
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2021-10-11 21:44:12 +0300

    fix b2b slideUp/Down

--- b2b/src/js/main.js
+++ b2b/src/js/main.js
@@ -3,41 +3,36 @@ let body = document.body,
 	printOrder = document.getElementById('print-order'),
 	headerTop = document.querySelector('.headerMainJs');
 
-const openDropdown = elem => {
+const openDropdown = (elem, duration) => {
 	let dropdownBody = elem.nextElementSibling;
-	const DURATION = 300;
-
 	elem.classList.add('open');
+	dropdownBody.classList.add('open');
 	dropdownBody.style.height = 'auto';
-
 	let originHeight = `${dropdownBody.clientHeight}px`;
-
-	dropdownBody.style.cssText = `height : 0px; transition-duration : ${DURATION / 1000}s`
+	dropdownBody.style.cssText = `height : 0px; transition-duration : ${duration / 1000}s`
 	setTimeout( () => dropdownBody.style.height = originHeight, 0);
 };
-
-const closeAll = elem => {
-	elem.forEach(function (block) {
-		block.classList.remove('open');
+const closeDropdown = (elem, duration) => {
+	elem.forEach(function (activeDropdownBtn) {
+		activeDropdownBtn.classList.remove('open')
+		activeDropdownBtn.nextElementSibling.style.height = 0;
+		setTimeout( () => activeDropdownBtn.nextElementSibling.classList.remove('open'), duration);
 	});
 };
-
-
-
 body.addEventListener('click', function (e) {
 	let dropdownBtn = e.target.closest('.dropdownBtnJs'),
-		openUi = document.querySelectorAll('.open');
+		activeDropdownBtns = document.querySelectorAll('.dropdownBtnJs.open');
 
 	if(dropdownBtn) {
 		if (!dropdownBtn.classList.contains('open')) {
-			closeAll(openUi);
-			openDropdown(dropdownBtn);
+			closeDropdown(activeDropdownBtns, 300);
+			openDropdown(dropdownBtn, 300);
 		}
 		else {
-			closeAll(openUi);
+			closeDropdown(activeDropdownBtns, 300);
 		}
-	} else if (openUi.length && !e.target.closest('.dropdownWrapJs')) {
-		closeAll(openUi);
+	} else if (activeDropdownBtns.length && !e.target.closest('.dropdownWrapJs')) {
+		closeDropdown(activeDropdownBtns, 300);
 	}
 });
 
