d892e7a353ab3c1d3809fe82bcb045fab851a163
IgorNichiporuk <igor.nichiporuk@autoset.by>
2021-10-19 17:31:03 +0300

    fix-1 hw-14

--- homework/lesson-22/hw-14/main.js
+++ homework/lesson-22/hw-14/main.js
@@ -1,67 +1,175 @@
-var deadline = 2; // minutes
 var timer = document.getElementById('timer'),
-	timerControl = timer.querySelector('.timer__control');
-
-
-
-function setTimer(total) {
-	total += 1;
-	localStorage.setItem('timerMilliseconds', total);
-	var min = Math.floor((total / 100 / 60) % 60),
-		sec = Math.floor((total / 100) % 60),
-		millisec = total % 100;
-	return {
-		min : min,
-		sec : sec,
-		millisec : millisec,
-	}
-}
-
-
-function initTimer() {
-	var minutes = document.getElementById('minutes'),
-		seconds = document.getElementById('seconds'),
-		milliseconds = document.getElementById('milliseconds');
-
-	var timeInterval = setInterval(function () {
-		var t = setTimer(+localStorage.getItem('timerMilliseconds'));
-		minutes.innerHTML = t.min < 10 ? '0' + t.min : t.min;
-		seconds.innerHTML = t.sec < 10 ? '0' + t.sec : t.sec;
-		milliseconds.innerHTML = t.millisec < 10 ? '0' + t.millisec : t.millisec;
-
-		if(t.min > 0) clearInterval(timeInterval);
-
-	}, 10);
-}
+	timerControl = timer.querySelector('.timerControlJs'),
+	timerList = timer.querySelector('.timerListJs'),
+	timerReset,
+	timerSave,
+	timerInterval,
+	deadline = 1; //минуты
 
 if(localStorage.getItem('timerInfo')) {
-	initTimer();
+	var t = stateTimer();
+	switch (t.status) {
+		case 'run':
+			startTimer(deadline);
+			showTimerBtns();
+			showTimerList();
+			break;
+		case 'stop':
+			getTimer();
+			showTimerBtns();
+			showTimerList();
+			break;
+		default:
+			resetTimer();
+	}
 }
-
 
 timerControl.onclick = function () {
 	switch (this.dataset.state) {
 		case 'init':
-			localStorage.setItem('timerInfo', JSON.stringify({
-				milleseconds : 0,
-				status : 'run',
-				text : 'Stop'
-			}));
-			var timerInfo = JSON.parse(localStorage.getItem('timerState'));
-			this.dataset.state = timerInfo.status;
-			this.innerText = timerInfo.text;
-			initTimer();
+			showTimerBtns();
+			startTimer(deadline);
+			break;
+		case 'stop':
+			startTimer(deadline);
 			break;
 		case 'run':
-			localStorage.setItem('timerMilliseconds', 0);
-			this.dataset.state = 'stop';
-			this.innerText = 'Start';
-			initTimer();
+			stopTimer();
 			break;
 	}
-	/*localStorage.setItem('timerMilliseconds', 0);
-	initTimer(timer, deadline);*/
 };
+
+timer.onclick = function (e) {
+	if (e.target.classList.contains('timerResetJs')) {
+		resetTimer();
+	} else if(e.target.classList.contains('timerSaveJs')) {
+		saveTimer();
+	}
+};
+
+function showTimerBtns() {
+	timerList.insertAdjacentHTML('beforebegin', '<button class="timer__control timerResetJs">Reset</button>'
+		+'<button class="timer__control timerSaveJs">Save</button>');
+}
+
+function showTimerList() {
+	var points = JSON.parse(localStorage.getItem('timerPointsList'));
+	if (points) {
+		points.forEach(function (point) {
+			timerList.insertAdjacentHTML('beforeend', '<li class="timer__list-item">' +point+'</li>');
+		});
+	}
+}
+
+function formatData(num) {
+	return num < 10 ? '0' + num : num;
+}
+
+function setTimer(total) {
+	var min = Math.floor((total / 100 / 60) % 60),
+		sec = Math.floor((total / 100) % 60),
+		millisec = total % 100;
+	return {
+		min : formatData(min),
+		sec : formatData(sec),
+		millisec : formatData(millisec)
+	}
+}
+
+function getTimer() {
+	var minutes = document.getElementById('minutes'),
+		seconds = document.getElementById('seconds'),
+		milliseconds = document.getElementById('milliseconds'),
+		t = setTimer(+localStorage.getItem('timerMilliseconds'));
+
+	minutes.innerHTML = t.min;
+	seconds.innerHTML = t.sec;
+	milliseconds.innerHTML = t.millisec;
+	return t;
+}
+
+function updateTimer(endtime) {
+	localStorage.setItem('timerMilliseconds', +localStorage.getItem('timerMilliseconds') + 1);
+
+	var t = getTimer();
+
+	if(t.min == endtime) {
+		clearInterval(timerInterval);
+		localStorage.setItem('timerInfo', JSON.stringify({
+			status : 'end',
+		}));
+		timerSave = timer.getElementsByClassName('timerSaveJs')[0];
+		timerSave.remove();
+		timerControl.remove();
+	}
+}
+
+function resetTimer() {
+	var timerWrap = timer.getElementsByClassName('timerWrapJs')[0];
+	timer.insertBefore(timerControl, timerWrap);
+	timerReset = timer.getElementsByClassName('timerResetJs')[0];
+	timerSave = timer.getElementsByClassName('timerSaveJs')[0];
+	if(timerReset) {
+		timerReset.remove();
+	}
+	if(timerSave) {
+		timerSave.remove();
+	}
+	timerList.innerHTML = '';
+	clearInterval(timerInterval);
+	localStorage.removeItem('timerMilliseconds');
+	localStorage.removeItem('timerPointsList');
+	localStorage.setItem('timerInfo', JSON.stringify({
+		status : 'init',
+		text : 'Start'
+	}));
+	stateTimer();
+	getTimer();
+}
+
+function saveTimer() {
+	var t = setTimer(+localStorage.getItem('timerMilliseconds'));
+	if (localStorage.getItem('timerPointsList')) {
+		var points = JSON.parse(localStorage.getItem('timerPointsList'));
+		points.push(t.min+ ' : '+t.sec+ ' : '+t.millisec);
+		localStorage.setItem('timerPointsList', JSON.stringify(points));
+	} else {
+		localStorage.setItem('timerPointsList', JSON.stringify([t.min+ ' : '+t.sec+ ' : '+t.millisec]));
+	}
+	timerList.insertAdjacentHTML('beforeend', '<li class="timer__list-item">' +t.min+ ' : '+t.sec+ ' : '+t.millisec+'</li>');
+}
+
+function startTimer(endtime) {
+	localStorage.setItem('timerInfo', JSON.stringify({
+		status : 'run',
+		text : 'Stop'
+	}));
+	stateTimer();
+	timerInterval = setInterval(updateTimer, 10, endtime);
+}
+
+function stopTimer() {
+	clearInterval(timerInterval);
+	localStorage.setItem('timerInfo', JSON.stringify({
+		status : 'stop',
+		text : 'Run'
+	}));
+	stateTimer();
+}
+
+function stateTimer() {
+	var timerInfo = JSON.parse(localStorage.getItem('timerInfo'));
+
+	timerControl.dataset.state = timerInfo.status;
+	timerControl.innerText = timerInfo.text;
+
+	return timerInfo;
+}
+
+
+
+
+
 
 
 
