cad4a70fa7a432cdaa14aa0a9957948cc33e4d09
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2021-11-06 19:35:57 +0300

    delete test spa_server

--- spa_dev_server_1/frontend/src/js/helpers/utils.js
+++
@@ -1,8 +0,0 @@
-export const parseRequestURL = () => {
-    const url = location.hash.slice(2),
-        request = {};
-
-    [request.resource, request.id, request.action] = url.split('/');
-
-    return request;
-};
